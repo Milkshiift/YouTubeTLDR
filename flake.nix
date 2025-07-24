a32184034cfe1b14bc14cde70fd5ac50db10e3d6
Milkshiift <99212308+Milkshiift@users.noreply.github.com>
2025-07-24 15:10:09 +0300

    Init service flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,97 @@
 {
-  description = "OpenSSL";
-
-  # I already have a system-wide rust toolchain so it's not included here
+  description = "YouTubeTLDR server";
 
   inputs = {
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-      flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    fenix = {
+        url = "github:nix-community/fenix";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.upx
-              pkgs.openssl
-              pkgs.pkg-config
-            ];
+  outputs = { self, nixpkgs, flake-utils, fenix, crane, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-            LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
+        rust-toolchain = fenix.packages.${system}.default.toolchain;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+
+        project = pkgs.lib.importTOML ./Cargo.toml;
+
+        youtubetldr = craneLib.buildPackage {
+          pname = project.package.name;
+          version = project.package.version;
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            mold
+          ];
+          cargoExtraArgs = "--no-default-features --features rustls-tls";
+        };
+      in
+      {
+        packages.default = youtubetldr;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ youtubetldr ];
+          nativeBuildInputs = with pkgs; [
+            openssl
+            pkg-config
+          ];
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
+        };
+      }) // {
+        nixosModules.default = { config, pkgs, ... }:
+          let
+            cfg = config.services.youtubetldr;
+          in
+          {
+            options.services.youtubetldr = {
+              enable = pkgs.lib.mkEnableOption "Enable the YouTubeTLDR server";
+
+              ip = pkgs.lib.mkOption {
+                type = pkgs.lib.types.str;
+                default = "0.0.0.0";
+                description = "IP address to bind to";
+              };
+
+              port = pkgs.lib.mkOption {
+                type = pkgs.lib.types.port;
+                default = 8000;
+                description = "Port to listen on";
+              };
+            };
+
+            config = pkgs.lib.mkIf cfg.enable {
+              systemd.services.youtubetldr = {
+                description = "YouTubeTLDR server";
+                after = [ "network.target" ];
+                wantedBy = [ "multi-user.target" ];
+
+                serviceConfig = {
+                  ExecStart = "${self.packages.${pkgs.system}.default}/bin/YouTubeTLDR";
+                  Restart = "on-failure";
+                  User = "youtubetldr";
+                  Group = "youtubetldr";
+                  Environment = [
+                    "TLDR_IP=${cfg.ip}"
+                    "TLDR_PORT=${toString cfg.port}"
+                  ];
+                };
+              };
+
+              users.users.youtubetldr = {
+                isSystemUser = true;
+                group = "youtubetldr";
+              };
+
+              users.groups.youtubetldr = {};
+            };
           };
-        });
+      };
 }
